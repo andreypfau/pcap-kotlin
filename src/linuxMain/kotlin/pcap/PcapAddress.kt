0b705859dc03dc53ed3ce3c351e3d4002729d8f5
Andrey Pfau <andreypfau@ton.org>
2022-09-24 14:21:36 +0300

    pcap for linux

--- src/linuxMain/kotlin/pcap/PcapAddress.kt
+++ src/linuxMain/kotlin/pcap/PcapAddress.kt
@@ -1,18 +1,18 @@
 package pcap
 
-import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.pointed
-import kotlinx.cinterop.readBytes
-import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.*
 import platform.linux.sockaddr_ll
-import platform.posix.AF_INET
-import platform.posix.AF_INET6
-import platform.posix.AF_PACKET
+import platform.posix.*
 
 actual fun PcapAddress(pointer: CPointer<sockaddr>): PcapAddress {
     return when (pointer.pointed.sa_family.toInt()) {
         AF_INET -> {
-            val data = pointer.readBytes(4)
+            val inet4 = pointer.reinterpret<sockaddr_in>().pointed
+            val data = memScoped {
+                val bytes = allocArray<ByteVar>(4)
+                memcpy(bytes, inet4.sin_addr.ptr, 4)
+                bytes.readBytes(4)
+            }
             PcapAddress(PcapAddress.AddressFamily.INET4, data)
         }
 
